7e61736f0b97616b61cba3903f465385b564b346
Ishaan Rawat <i.rawat98@gmail.com>
2022-10-16 00:51:24 +0100

    data being retrieved from api call

--- weatherApp/script.js
+++ weatherApp/script.js
@@ -4,14 +4,24 @@ let weather = {
         fetch(
             "https://api.openweathermap.org/data/2.5/weather?q=" 
             + city  
-            + "&units=metrics&appid=" 
+            + "&appid=" 
             + this.apiKey
         )
             .then((response) => response.json())
-            .then((data) => console.log(data));
+            .then((data) => this.displayWeather(data));
     },
     displayWeather: function(data) {
-
+        const {name} = data;
+        const {icon,description} = data.weather[0];
+        const {temp,humidity} = data.main;
+        const {speed} = data.wind;
+        console.log(name,icon,description,temp,humidity,speed);
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src ="http://openweathermap.org/img/w/" + icon + ".png";       
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity;
+        document.querySelector(".wind").innerText = "Wind Speed: " + speed;
     }
 };
     
