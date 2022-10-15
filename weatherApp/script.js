8b90afa46cf7554ccfcd4ab4b09ec969af00d373
Ishaan Rawat <i.rawat98@gmail.com>
2022-10-15 23:48:40 +0100

    attempting to fetch weather and display

---
+++ weatherApp/script.js
@@ -0,0 +1,17 @@
+let weather = {
+    "apiKey": "628b4e8ad6db4b8b4434648c1ad74aa4",
+    fetchWeather: function (city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q=" 
+            + city  
+            + "&appid=" 
+            + appKey;
+        )
+            .then((response) => response.json())
+            .then((data) => console.log(data));
+    },
+    displayWeather: function(data) {
+
+    }
+};
+    
