cd0f3c8c905ead3a061bd1c66076289c5fc515de
Ishaan Rawat <i.rawat98@gmail.com>
2022-10-16 00:02:41 +0100

    data is being fetched through console

--- weatherApp/script.js
+++ weatherApp/script.js
@@ -1,11 +1,11 @@
 let weather = {
-    "apiKey": "628b4e8ad6db4b8b4434648c1ad74aa4",
+    apiKey: "628b4e8ad6db4b8b4434648c1ad74aa4",
     fetchWeather: function (city) {
         fetch(
             "https://api.openweathermap.org/data/2.5/weather?q=" 
             + city  
-            + "&appid=" 
-            + appKey;
+            + "&units=metrics&appid=" 
+            + this.apiKey
         )
             .then((response) => response.json())
             .then((data) => console.log(data));
